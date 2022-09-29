69e1b06e56cb86d36a6656def79a8c2445a2d7f2
kelly <kellydakine@gmail.com>
2022-09-29 16:26:07 -0300

    Inserindo as cores utilizadas

---
+++ src/UI/variable.jsx
@@ -0,0 +1,8 @@
+export const backgroundLogin = "linear-gradient(180deg, #33383D 0%, #1C1D20)";
+export const primaryButton = "linear-gradient(90deg, #FF2D04 0%, #C13216 100%)";
+export const mainColor = "#E0E0E0";
+export const whiteColor = "#FFFFFF";
+export const secondaryColor = "#E9B425";
+export const titleColor = "#C12D18";
+export const titleColorHome = "#222222";
+export const backgroundHome = "linear-gradient(90.16deg, #ffffff 0%, #e0e0e0 100%)";
