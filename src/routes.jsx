50ec854330d2e15bc637d7e0ec82392b2418dfcb
kelly <kellydakine@gmail.com>
2022-09-29 17:14:32 -0300

    Inserindo as rotas das páginas

---
+++ src/routes.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
